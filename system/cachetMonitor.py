412b0741d843c15fc2e2df4dcdac5b3a357e1f27
Phoenix09 <Phoenix09@users.noreply.github.com>
2018-04-08 04:18:59 +0100

    Ability to set separate target URL for monitoring
    Other URL will be used in status messages if target is set

--- system/cachetMonitor.py
+++ system/cachetMonitor.py
@@ -109,6 +109,10 @@ class Cachet(object):
             isEnabled = self.config['monitoring'][x]['enabled']
             status_codes = self.config['monitoring'][x]['expected_status_code']
             url = self.config['monitoring'][x]['url']
+            try:
+                target = self.config['monitoring'][x]['target']
+            except KeyError:
+                target = url
             request_method = self.config['monitoring'][x]['method']
             c_id = self.config['monitoring'][x]['component_id']
             localtime = time.asctime(time.localtime(time.time()))
@@ -127,7 +131,7 @@ class Cachet(object):
             try:
                 if isEnabled:
                     if request_method.lower() == "get":
-                        r = requests.get(url, verify=strict_tls, timeout=check_timeout, headers=headers)
+                        r = requests.get(target, verify=strict_tls, timeout=check_timeout, headers=headers)
                         # self.utils.postMetricsPointsByID(1, r.elapsed.total_seconds() * 1000)
                         if r.status_code not in status_codes and r.status_code not in self.httpErrors:
                             error_code = '%s check **failed** - %s \n\n`%s %s HTTP StatusError: %s`' % (
@@ -150,7 +154,7 @@ class Cachet(object):
                                 self.utils.putComponentsByID(c_id, status=c_status)
                             self.logs.warn("%s" % error_code.replace('\n', '').replace('`', ''))
                     elif request_method.lower() == "post":
-                        r = requests.get(url, verify=strict_tls, timeout=check_timeout, headers=headers)
+                        r = requests.get(target, verify=strict_tls, timeout=check_timeout, headers=headers)
                         if r.status_code not in status_codes and r.status_code not in self.httpErrors:
                             error_code = '%s check **failed** - %s \n\n`%s %s HTTP Status Error: %s`' % (
                             url, localtime, request_method, url, httplib.responses[r.status_code])
